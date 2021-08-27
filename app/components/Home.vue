9d67bf74f943e6f98ef3f7746ce924c3f1becfcc
Adamson Jeremiah <jaletechs@gmail.com>
2021-08-27 22:57:43 +0200

    Initial commit

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,38 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
-
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-    </Page>
+  <Page class="bg-blue-100">
+    <ActionBar class="text-white bg-blue-900" title="Hello Nativescript-Vue" />
+    <GridLayout>
+      <Label class="info">
+        <FormattedString class="bg-blue-500">
+          <Span class="fas" text.decode="&#xf135; " />
+          <Span :text="message" />
+        </FormattedString>
+      </Label>
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
+export default {
+  computed: {
+    message() {
+      return "Blank {N}-Vue app";
     }
-  };
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+@import "@nativescript/theme/scss/variables/blue";
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+  @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+  font-size: 20;
+  horizontal-align: center;
+  vertical-align: center;
+}
 </style>
